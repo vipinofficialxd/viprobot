221749f927aee9e7a538709ab561dbb107a8aa91
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-04-29 08:26:18 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,20 +14,20 @@ class Config(object):
     MONGO_DB_URI = "mongodb+srv://starxrobo:starxrobo@cluster0.efstcnr.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://graph.org/file/04e7a29075701e7a68ca5.jpg"
+    START_IMG = "https://telegra.ph/file/b8ec8da5949144c58aa89.jpg"
 
-    SUPPORT_CHAT = "TH3ONLYSUPPORT"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "DabangTheBrand"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = "G1VE375UCXVX"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = "5307865914"  # User id of your telegram account (Must be integer)
+    OWNER_ID = "5999224089"  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
+    DEV_USERS = [5765818401]  # User id of dev users
     DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
     WOLVES = []  # User id of whitelist users

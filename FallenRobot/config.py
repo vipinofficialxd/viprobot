f98497c50b578314900ceb0384b86d5f79ed0649
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-07-02 12:24:38 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -25,12 +25,12 @@ class Config(object):
     OWNER_ID = "5999224089"  # User id of your telegram account (Must be integer)
 
     # Optional fields
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = "5765818401"  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    BL_CHATS = [] # List of groups that you want blacklisted.
+    DRAGONS = "6144223366" # User id of sudo users
+    DEV_USERS = "6144223366"   # User id of dev users
+    DEMONS = "6144223366" # User id of support users
+    TIGERS = "6144223366"  # User id of tiger users
+    WOLVES = "6144223366"  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
